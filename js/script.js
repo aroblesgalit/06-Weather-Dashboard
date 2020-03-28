9dd3690f1d7f4c4068b214651db90be478bd7e01
Alvin Galit <aroblesgalit@gmail.com>
2020-03-28 18:04:18 -0500

    Added another ajax call for getting the uv index data and rendering it to the DOM

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
 
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + inputValue + "&appid=" + APIKey;
 
+
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -20,8 +21,14 @@ $(document).ready(function () {
             var curFahrenheit = ((curKelvinTemp - 273.15) * 1.80 + 32).toFixed();
             var curHumidity = response.main.humidity;
             var curWindSpeed = response.wind.speed;
-            var curWeatherIcon = "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png" ;
+            var curWeatherIcon = "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
             var curWeatherDes = response.weather[0].description;
+            var cityLat = response.coord.lat;
+            var cityLon = response.coord.lon;
+            // var cityCoord = [cityLat, cityLon];
+
+            // // Store cityLat and cityLon to localStorage
+            // localStorage.setItem("cityCoord", cityCoord);
 
             renderData();
 
@@ -29,7 +36,7 @@ $(document).ready(function () {
             function renderData() {
                 // Create divs and add classes
                 var currentWeatherData = $("<div>").addClass("uk-grid");
-                var currentDataText = $("<div>").addClass("uk-width-2-3 uk-width-1-2@s uk-text-secondary");
+                var currentDataText = $("<div>").addClass("uk-width-2-3 uk-width-1-2@s uk-text-secondary dataText");
                 var currentDataGraphic = $("<div>").addClass("uk-width-1-3 uk-width-1-2@s weatherGraphic");
                 var currentDataCity = $("<div>").addClass("uk-text-bold uk-text-large");
                 var currentDataDate = $("<div>");
@@ -41,9 +48,6 @@ $(document).ready(function () {
                 var currentDataWindSpeed = $("<div>");
                 var currentWindSpeedLabel = $("<span>").addClass("uk-text-small");
                 var currentWindSpeedVal = $("<span>").addClass("uk-text-bold");
-                var currentDataUVIndex = $("<div>");
-                var currentUVIndexLabel = $("<span>").addClass("uk-text-small");
-                var currentUVIndexVal = $("<span>").addClass("uk-text-bold");;
                 var currentWeatherImage = $("<img>");
                 var currentWeatherDescription = $("<p>").addClass("uk-text-small uk-light uk-margin-remove uk-padding-large uk-padding-remove-vertical");
                 // Set contexts
@@ -55,21 +59,53 @@ $(document).ready(function () {
                 currentHumidityVal.text(curHumidity + "%");
                 currentWindSpeedLabel.text("Wind Speed: ");
                 currentWindSpeedVal.text(curWindSpeed + "kph");
-                currentWeatherImage.attr({"data-src": curWeatherIcon, alt: "Weather Icon", "uk-img": "", width: "200px"});
+                currentWeatherImage.attr({ "data-src": curWeatherIcon, alt: "Weather Icon", "uk-img": "", width: "200px" });
                 // currentWeatherImage.attr({"data-src": curWeatherIcon, alt: "Weather Icon", width: "200px"});
                 currentWeatherDescription.text(curWeatherDes);
 
                 // Append
-                currentDataUVIndex.append(currentUVIndexLabel).append(currentUVIndexVal);
                 currentDataWindSpeed.append(currentWindSpeedLabel).append(currentWindSpeedVal);
                 currentDataHumidity.append(currentHumidityLabel).append(currentHumidityVal);
                 currentDataTemp.append(currentDataTempUnit);
-                currentDataText.append(currentDataCity).append(currentDataDate).append(currentDataTemp).append(currentDataHumidity).append(currentDataWindSpeed).append(currentDataUVIndex);
+                currentDataText.append(currentDataCity).append(currentDataDate).append(currentDataTemp).append(currentDataHumidity).append(currentDataWindSpeed);
                 currentDataGraphic.append(currentWeatherImage).append(currentWeatherDescription);
                 currentWeatherData.append(currentDataText).append(currentDataGraphic);
                 $(".todayWeatherSection").append(currentWeatherData);
             }
+
+            // var cityCoord = localStorage.getItem("cityCoord");
+            var queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + cityLat + "&lon=" + cityLon;
+            // Find the UV Index
+            $.ajax({
+                url: queryURL2,
+                method: "GET"
+            }).then(function(response) {
+                var uvIndexVal = response.value;
+
+                renderUVIndex();
+                // Render uv index
+                function renderUVIndex() {
+                    var currentDataText = $(".dataText");
+                    
+                    // Create divs
+                    var currentDataUVIndex = $("<div>");
+                    var currentUVIndexLabel = $("<span>").addClass("uk-text-small");
+                    var currentUVIndexVal = $("<span>").addClass("uk-text-bold");
+
+                    // Set text context
+                    currentUVIndexLabel.text("UV Index: ");
+                    currentUVIndexVal.text(uvIndexVal);
+
+                    // Append
+                    currentDataUVIndex.append(currentUVIndexLabel).append(currentUVIndexVal);
+                    currentDataText.append(currentDataUVIndex);
+                }
+            })
+
         })
     })
+
+
+
 
 })
