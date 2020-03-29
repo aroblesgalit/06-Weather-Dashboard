81e1411cc38673bfb1036b3cc9cf3117ca294897
Alvin Galit <aroblesgalit@gmail.com>
2020-03-29 00:05:16 -0500

    Finished logic for getting the 5day forecast and rendering it to the screen

--- js/script.js
+++ js/script.js
@@ -82,7 +82,7 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-
+            console.log(response);
             // Use response to get the necessary data: City name, Temperature in Fahrenheit, Humidity, Wind speed, and Coordinates (to use for UV Index)
             var curCity = response.name;
             var curDateTime = moment().format('MMM Do, h:mm a'); // Get current time using moment.js
@@ -148,7 +148,7 @@ $(document).ready(function () {
             }).then(function (response) {
                 // Get the UV Index value and store in a variable
                 var uvIndexVal = response.value;
-
+                console.log(response);
                 // Render the UV Index and display on screen
                 renderUVIndex();
 
@@ -193,38 +193,73 @@ $(document).ready(function () {
                 url: queryURL3,
                 method: "GET"
             }).then(function (response) {
-                // Get the current hour using moment.js in the 24 hour format
+                // Get the current hour using moment.js in the 24 hour format -- If I want to use more logic
+                // var currentHour = moment().format('H');
+
+                // Target fiveDaySection
+                var fiveDaySection = $(".fiveDaySection");
 
                 // Get list array
                 var listArray = response.list;
 
-                // Create a function to render data 
+                // Use to get data
+                var forecastWeekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+                var forecastMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+
+                renderForecast();
+                // Create a function to render data for the forecast
                 function renderForecast() {
                     // Loop through array
                     for (var i = 0; i < listArray.length; i++) {
                         // Find the date and time data
-                        var date = listArray[i].dt_txt;
+                        var futureDate = listArray[i].dt_txt;
                         // Create an instance of each date and time
-                        var d = new Date(date);
+                        var dateInstance = new Date(futureDate);
                         // Get the hours
-                        var hr = d.getHours();
+                        var futureHour = dateInstance.getHours();
                         // If the hours is greater than the current hour
-                        if (hr === 12) {
+                        if (futureHour === 12) {
                             // Then get the date, weather icon, temperature, humidity
-                            console.log(listArray[i]);
+                            // Turn date to format Saturday, Mar 28
+                            var forecastDay = forecastWeekdays[dateInstance.getDay()];
+                            var forecastMonth = forecastMonths[dateInstance.getMonth()];
+                            var forecastDate = dateInstance.getDate();
+                            var forecastDayDate = forecastDay + ", " + forecastMonth + " " + forecastDate;
+                            var forecastIcon = listArray[i].weather[0].icon;
+                            var forecastIconUrl = "https://openweathermap.org/img/wn/" + forecastIcon + "@2x.png";
+                            var forecastTempK = listArray[i].main.temp;
+                            var forecastHumidity = listArray[i].main.humidity;                           
+                            // Convert temp to F
+                            var forecastTempF = ((forecastTempK - 273.15) * 1.80 + 32).toFixed();
                             // Create divs and add classes
-
+                            var forecastCardWrapper = $("<div>").addClass("uk-width-1-5@m uk-width-1-3@s uk-light forecastCardWrapper");
+                            var forecastCard = $("<div>").addClass("uk-card uk-card-body uk-flex uk-flex-column uk-flex-middle forecastCard");
+                            var forecastDateSpan = $("<span>").addClass("uk-text-bold forecastDate");
+                            var forecastIconImg = $("<img>");
+                            var forecastTempDiv = $("<div>").addClass("uk-heading-medium uk-margin-remove");
+                            var forecastTempUnitSpan = $("<span>").addClass("uk-heading-small uk-text-top");
+                            var forecastHumidityDiv = $("<div>").addClass("humidityForecast");
+                            var forecastHumidityLabel = $("<span>").addClass("uk-text-small");
+                            var forecastHumidityVal = $("<span>").addClass("uk-text-bold");
                             // Set the contexts
+                            forecastDateSpan.text(forecastDayDate);
+                            forecastIconImg.attr({"data-src": forecastIconUrl, alt: "Weather icon", width: "40px", "uk-img": ""});
+                            forecastTempDiv.text(forecastTempF);
+                            forecastTempUnitSpan.html("&#176F");
+                            forecastHumidityLabel.text("Humidity: ");
+                            forecastHumidityVal.text(forecastHumidity + "%");
 
                             // Append
+                            forecastHumidityDiv.append(forecastHumidityLabel).append(forecastHumidityVal);
+                            forecastTempDiv.append(forecastTempUnitSpan);
+                            forecastCard.append(forecastDateSpan).append(forecastIconImg).append(forecastTempDiv).append(forecastHumidityDiv);
+                            forecastCardWrapper.append(forecastCard);
+                            fiveDaySection.append(forecastCardWrapper);
                         }
 
                     }
 
-                    // var d = new Date(date);
-                    // console.log(d.getHours()); // => 9
-                    // console.log(d.getMinutes()); // =>  30
-                    // console.log(d.getSeconds()); // => 51
                 }
 
             })
