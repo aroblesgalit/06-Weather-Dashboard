ead6e4ced0fdfb586ceb1bc616adbb3a8b1ebfe0
Alvin Galit <aroblesgalit@gmail.com>
2020-03-30 08:45:43 -0500

    Requested the city's country and displayed next to city

--- js/script.js
+++ js/script.js
@@ -129,9 +129,10 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
+
             // Use response to get the necessary data: City name, Temperature in Fahrenheit, Humidity, Wind speed, and Coordinates (to use for UV Index)
             var curCity = response.name;
+            var curCountry = response.sys.country;
             // var curDateTime = moment().format('MMM Do, h:mm a'); // Get current time using moment.js
             var curKelvinTemp = response.main.temp;
             var curFahrenheit = ((curKelvinTemp - 273.15) * 1.80 + 32).toFixed();
@@ -171,8 +172,7 @@ $(document).ready(function () {
                 }
 
                 // Set contexts
-                currentDataCity.text(curCity); // Get city from response
-                // currentDataDate.text(curDateTime); // Use moment.js
+                currentDataCity.text(curCity + ", " + curCountry); // Get city from response
                 currentDataTemp.text(curFahrenheit); // Get temp from response
                 currentDataTempUnit.html("&#176F");
                 currentHumidityLabel.text("Humidity: ");
@@ -227,52 +227,51 @@ $(document).ready(function () {
                     currentDataText.prepend(currentDataDate);
                 })
 
-            }
 
+                // Store the queryURL needed to get the data for UV Index
+                var queryURL2 = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + cityLat + "&lon=" + cityLon;
+                // Use a separate ajax call for getting the UV Index with a different queryURL
+                $.ajax({
+                    url: queryURL2,
+                    method: "GET"
+                }).then(function (response) {
+                    // Get the UV Index value and store in a variable
+                    var uvIndexVal = response.value;
+                    // Render the UV Index and display on screen
+                    renderUVIndex();
 
-            // Store the queryURL needed to get the data for UV Index
-            var queryURL2 = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + cityLat + "&lon=" + cityLon;
-            // Use a separate ajax call for getting the UV Index with a different queryURL
-            $.ajax({
-                url: queryURL2,
-                method: "GET"
-            }).then(function (response) {
-                // Get the UV Index value and store in a variable
-                var uvIndexVal = response.value;
-                // Render the UV Index and display on screen
-                renderUVIndex();
+                    // Create a function that would render the UV Index
+                    function renderUVIndex() {
+                        // Target the div where to append the UV Index
+                        var currentDataText = $(".dataText");
 
-                // Create a function that would render the UV Index
-                function renderUVIndex() {
-                    // Target the div where to append the UV Index
-                    var currentDataText = $(".dataText");
+                        // Create divs and add class
+                        var currentDataUVIndex = $("<div>").addClass("otherInfo");
+                        var currentUVIndexLabel = $("<span>").addClass("uk-text-small");
+                        var currentUVIndexVal = $("<span>").addClass("uk-text-bold uvIndex");
+                        // Set contexts
+                        currentUVIndexLabel.text("UV Index: ");
+                        currentUVIndexVal.text(uvIndexVal);
+                        // Create conditionals for color coding
+                        if (uvIndexVal >= 0 && uvIndexVal < 3) {
+                            currentUVIndexVal.css("color", "var(--very-low)");
+                        } else if (uvIndexVal >= 3 && uvIndexVal < 5) {
+                            currentUVIndexVal.css("color", "var(--low)");
+                        } else if (uvIndexVal >= 5 && uvIndexVal < 7) {
+                            currentUVIndexVal.css("color", "var(--moderate)");
+                        } else if (uvIndexVal >= 7 && uvIndexVal < 10) {
+                            currentUVIndexVal.css("color", "var(--high)");
+                        } else if (uvIndexVal >= 10) {
+                            currentUVIndexVal.css("color", "var(--very-high)");
+                        }
 
-                    // Create divs and add class
-                    var currentDataUVIndex = $("<div>").addClass("otherInfo");
-                    var currentUVIndexLabel = $("<span>").addClass("uk-text-small");
-                    var currentUVIndexVal = $("<span>").addClass("uk-text-bold uvIndex");
-                    // Set contexts
-                    currentUVIndexLabel.text("UV Index: ");
-                    currentUVIndexVal.text(uvIndexVal);
-                    // Create conditionals for color coding
-                    if (uvIndexVal >= 0 && uvIndexVal < 3) {
-                        currentUVIndexVal.css("color", "var(--very-low)");
-                    } else if (uvIndexVal >= 3 && uvIndexVal < 5) {
-                        currentUVIndexVal.css("color", "var(--low)");
-                    } else if (uvIndexVal >= 5 && uvIndexVal < 7) {
-                        currentUVIndexVal.css("color", "var(--moderate)");
-                    } else if (uvIndexVal >= 7 && uvIndexVal < 10) {
-                        currentUVIndexVal.css("color", "var(--high)");
-                    } else if (uvIndexVal >= 10) {
-                        currentUVIndexVal.css("color", "var(--very-high)");
+                        // Append
+                        currentDataUVIndex.append(currentUVIndexLabel).append(currentUVIndexVal);
+                        currentDataText.append(currentDataUVIndex);
                     }
+                })
 
-                    // Append
-                    currentDataUVIndex.append(currentUVIndexLabel).append(currentUVIndexVal);
-                    currentDataText.append(currentDataUVIndex);
-                }
-            })
-
+            }
 
 
             // Store the queryURL needed to get the data for the 5-day forecast
